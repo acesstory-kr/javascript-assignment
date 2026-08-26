867c238fff165168bd77db6cf484fe604b9a4124
acesstory-kr <acesstory@gmail.com>
2026-08-26 18:15:16 +0900

    Feat:js데이터를 객체로만들기

---
+++ Day12/script.js
@@ -0,0 +1,18 @@
+console.log("java start!")
+
+    
+const movie ={
+    id : 1,
+title :"인셉션",
+voteAverage:8.4 ,
+voteCount:35000 ,
+releaseDate:"2010-07-15" ,
+isFavorite:"false" ,
+};
+
+
+  console.log(movie.title);
+  console.log(movie.voteAverage);
+  console.log(movie.isFavorite);
+
+
