61d3074c2a3acc8ddb66d335f8cd2db47e264e8b
acesstory-kr <acesstory@gmail.com>
2026-08-31 19:03:24 +0900

    Feat:day16과제

---
+++ Day16/script.js
@@ -0,0 +1,29 @@
+// const text = JSON.stringify({ title: "Inception"});
+// // 인수를 받는데 객체로 받는다. 무슨뜻?
+
+// const movie=JSON.parse(text);
+// console.log(movie.title)
+
+// 02. JavaScript 객체 만들기
+const movie= {
+    id:101,
+    title:"Inception",
+    voteAverage:8.4,
+    isAdult:false,
+};
+
+console.log(movie);
+
+// 03. JavaScript 객체를 JSON 문자열로 변환하기
+const movieJson = JSON.stringify(movie);
+
+console.log(movieJson);
+console.log(typeof movieJson);
+
+
+// 04. JSON 문자열을 JavaScript 값으로 변환하기
+const parsedMovie = JSON.parse(movieJson);
+console.log(parsedMovie);
+console.log(typeof parsedMovie);
+
+// 05. Network에서 HTTP 통신 확인하기
